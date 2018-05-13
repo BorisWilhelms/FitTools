4e933667e191c8cbf90c947e584205fab9d0e98c
Boris Wilhelms <mail@wille-zone.de>
2018-05-13 16:32:05 +0200

    Add project files.

---
+++ FitTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FitTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
