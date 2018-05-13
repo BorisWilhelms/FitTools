b2e7158c01a94fef5680d1b39130b29eb7ed6fad
Boris Wilhelms <mail@wille-zone.de>
2018-05-13 18:18:17 +0200

    Adds Importer and Cleanup

---
+++ src/FitTools.Cleanup/Program.cs
@@ -0,0 +1,45 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Fitness.v1;
+using Google.Apis.Services;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitTools.Cleanup
+{
+    class Program
+    {
+        private const string VeryfitWeight = "raw:com.google.weight:com.veryfit2hr.second:";
+
+        static async Task Main(string[] args)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { FitnessService.Scope.FitnessBodyWrite },
+                    "user", CancellationToken.None);
+            }
+
+            var service = new FitnessService(new BaseClientService.Initializer() { HttpClientInitializer = credential, ApplicationName = "Fit Cleanup" });
+            var dataSource = service.Users.DataSources.DataPointChanges.List("me", VeryfitWeight).Execute();
+
+            Console.WriteLine($"Got {dataSource.InsertedDataPoint.Count} points");
+
+
+            var startTime = dataSource.InsertedDataPoint.Min(p => p.StartTimeNanos);
+            var endTime = dataSource.InsertedDataPoint.Max(p => p.EndTimeNanos);
+            if (startTime.HasValue && endTime.HasValue)
+            {
+                Console.WriteLine("Deleting points");
+                var response = service.Users.DataSources.Datasets.Delete("me", VeryfitWeight, $"{startTime.Value}-{endTime.Value}").Execute();
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/FitTools.Importer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Fitness.v1;
+using Google.Apis.Fitness.v1.Data;
+using Google.Apis.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FitTools
+{
+    class Program
+    {
+        private const string WeightUserInput = "raw:com.google.weight:com.google.android.apps.fitness:user_input";
+
+        static async Task Main(string[] args)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { FitnessService.Scope.FitnessBodyWrite },
+                    "user", CancellationToken.None);
+            }
+
+            var service = new FitnessService(new BaseClientService.Initializer() { HttpClientInitializer = credential, ApplicationName = "Fit Importer" });
+            var points = JsonConvert.DeserializeObject<List<DataPoint>>(File.ReadAllText("import.json"));
+            var dataSet = new Dataset()
+            {
+                DataSourceId = WeightUserInput,
+                MaxEndTimeNs = points.Max(p => p.EndTimeNanos.Value),
+                MinStartTimeNs = points.Min(p => p.StartTimeNanos.Value),
+                Point = points
+            };
+            Console.WriteLine($"Importing {dataSet.Point.Count} data points");
+            var response = service
+                .Users
+                .DataSources
+                .Datasets
+                .Patch(dataSet, "me", WeightUserInput, $"{points.Min(p => p.StartTimeNanos.Value)}-{points.Max(p => p.EndTimeNanos.Value)}")
+                .Execute();
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
